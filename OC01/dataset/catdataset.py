ead588b98e548a8aaf5477dcaecb0c766624e931
luhouyang <luhouyang@gmail.com>
2025-03-19 03:10:56 +0800

    feat: dataset complete

---
+++ OC01/dataset/catdataset.py
@@ -0,0 +1,188 @@
+"""
+Cat Dataset (CatDataset)
+
+author: Lu Hou Yang
+GitHub: https://github.com/luhouyang/open_circles.git
+date: 19 March 2025
+"""
+
+from pathlib import Path
+import pickle
+from typing import Callable, Optional
+
+import numpy as np
+import polars as pl
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+
+class CatDataset(Dataset):
+
+    def __init__(
+        self,
+        root: str,
+        split: str = 'train',
+        format: str = 'parquet',
+        transform: Optional[Callable] = None,
+        image_size=[224, 224],
+        image_channels: int = 3,
+        mask_channels: int = 1,
+    ):
+        """Feral cats segmentation dataset"""
+        super(CatDataset, self).__init__()
+
+        allowed_formats = [
+            'parquet',
+            'pkl',
+        ]
+
+        split_selection = ['train', 'val']
+
+        if format not in allowed_formats:
+            raise ValueError(
+                f'Selection {format} is not valid. Choose from: {" | ".join(allowed_formats)}'
+            )
+
+        if split not in split_selection:
+            raise ValueError(
+                f'Split {split} is not valid. Choose from: {" | ".join(split_selection)}'
+            )
+
+        root_path = Path(root)
+        if not root_path.exists():
+            raise ValueError(f'Directory {root} does not exist.')
+
+        if format == 'parquet':
+            data_path = root_path / f"{split}_dataset.parquet"
+            if not data_path.exists():
+                raise ValueError(f'File {data_path} not found.')
+
+            dataset = pl.read_parquet(data_path).to_numpy()
+
+            split_at = image_size[0] * image_size[1] * image_channels
+
+            # DO NOT delete formatting comments starting with 'yapf'
+            # yapf: disable
+            self.data = (dataset[:, :split_at]
+                .astype('float32')
+                .reshape(-1, image_size[0], image_size[1], image_channels)
+                )
+
+            self.masks = (dataset[:, split_at:]
+                .reshape(-1, image_size[0], image_size[1], mask_channels)
+                )
+            # yapf: enable
+
+        elif format == 'pkl':
+            data_path = root_path / f"{split}_dataset.pkl"
+            if not data_path.exists():
+                raise ValueError(f'File {data_path} not found.')
+
+            with open(data_path, 'rb') as f:
+                dataset = pickle.load(f)
+
+            images = [pickle.loads(img) for img, mask in dataset]
+            masks = [pickle.loads(mask) for img, mask in dataset]
+
+            self.data = np.stack(images).astype(np.float32)
+            self.masks = np.stack(masks).astype(np.uint8)
+
+        self.transform = transform if transform != None else transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[
+                        0.48235,
+                        0.45882,
+                        0.40784,
+                    ],
+                    std=[
+                        0.00392156862745098,
+                        0.00392156862745098,
+                        0.00392156862745098,
+                    ],
+                ),
+            ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return (
+            self.transform(self.data[index]),
+            torch.tensor(self.masks[index], dtype=torch.long),
+        )
+
+
+if __name__ == '__main__':
+    import timeit
+
+    root = r"D:\storage\feral-cat-segmentation.v1i.sam2"
+    image_size = [224, 224]
+    image_channels = 3
+    mask_channels = 1
+
+    def getds():
+        return CatDataset(
+            root=root,
+            split='train',
+            format='pkl',
+            image_size=image_size,
+            image_channels=image_channels,
+            mask_channels=mask_channels,
+        )
+
+    # 5 iterations
+    # parquet:  6.492156499996781   seconds
+    # pkl:      3.5605880999937654  seconds
+    print(timeit.timeit("getds()", globals=globals(), number=5))
+
+    ds = getds()
+
+    print(ds.__len__())
+    """Visualization code was generated with GenAI"""
+    import matplotlib.pyplot as plt
+
+    # Unnormalize function
+    def unnormalize(image, mean, std):
+        image = image.numpy().transpose(1, 2, 0)
+        image = (image * np.array(std)) + np.array(mean)
+        image = np.clip(image, 0, 255)
+        return image
+
+    # Normalization parameters (same as in dataset)
+    mean = [0.48235, 0.45882, 0.40784]
+    std = [0.00392156862745098] * 3  # Same std for all channels
+
+    # Select 9 random samples
+    num_samples = 9
+    indices = list(range(min(num_samples, len(ds))))
+
+    # Create a 3x6 grid (image-mask pairs)
+    fig, axes = plt.subplots(3, 6, figsize=(12, 6))
+
+    for i, idx in enumerate(indices):
+        image, mask = ds[idx * 3]
+
+        # Unnormalize image
+        image = unnormalize(image, mean, std).astype('int64')
+
+        # Convert mask to numpy
+        mask = mask.squeeze().numpy()
+
+        # Plot image
+        axes[i // 3, (i % 3) * 2].imshow(image)
+        axes[i // 3, (i % 3) * 2].set_title(f"Image {idx*3}")
+        axes[i // 3, (i % 3) * 2].axis("off")
+
+        # Plot mask
+        axes[i // 3, (i % 3) * 2 + 1].imshow(mask, cmap="gray")
+        axes[i // 3, (i % 3) * 2 + 1].set_title(f"Mask {idx*3}")
+        axes[i // 3, (i % 3) * 2 + 1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
--- OC01/preprocess/catpp.py
+++ OC01/preprocess/catpp.py
@@ -12,17 +12,92 @@ date: 18 March 2025
 import os
 from pathlib import Path
 import json
+import pickle
 
 from PIL import Image
 import numpy as np
 import polars as pl
 from pycocotools import mask as mask_utils
+from tqdm import tqdm
 
 import torch
 import torchvision.transforms as transforms
 
 
-def preprocess_catpp(root, target_size):
+def process_segmentation(json_path):
+    """This function was written with help from GenAI"""
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+
+    annotations = data.get('annotations', [])
+    if not annotations:
+        print(f"Error: No annotations found in {json_path}. Skipping...")
+        return [], []  # Skip processing this image
+
+    masks = []
+    mask = []
+    resized_annotations = []
+
+    image_info = data.get('image', {})
+    image_id = image_info.get('image_id')
+    file_name = image_info.get('file_name')
+    original_height = image_info.get('height')
+    original_width = image_info.get('width')
+
+    resize_factor_x = target_size[0] / original_width
+    resize_factor_y = target_size[1] / original_height
+
+    for annotation in annotations:
+        annotation_id = annotation.get('id')
+
+        # Decode RLE segmentation
+        rle = annotation.get('segmentation')
+        binary_mask = mask_utils.decode(rle)
+
+        # Convert to PIL Image and resize
+        mask_image = Image.fromarray(binary_mask.astype(np.uint8) * 255)
+        resized_mask = mask_image.resize(target_size, Image.NEAREST)
+
+        # Convert back to numpy array and normalize
+        resized_mask_array = np.array(resized_mask) / 255
+        # masks.append(resized_mask_array)
+
+        # Resize bounding box
+        original_bbox = annotation.get('bbox')
+        resized_bbox = [
+            original_bbox[0] * resize_factor_x,
+            original_bbox[1] * resize_factor_y,
+            original_bbox[2] * resize_factor_x,
+            original_bbox[3] * resize_factor_y
+        ]
+
+        # Update annotation with resized values
+        resized_annotation = annotation.copy()
+        resized_annotation['bbox'] = resized_bbox
+        resized_annotation['area'] = annotation.get(
+            'area') * resize_factor_x * resize_factor_y
+
+        # Create RLE for the resized mask
+        resized_mask_binary = resized_mask_array.astype(np.uint8)
+        masks.append(resized_mask_binary)
+        resized_rle = mask_utils.encode(np.asfortranarray(resized_mask_binary))
+        resized_rle['counts'] = resized_rle['counts'].decode('utf-8')
+        resized_annotation['segmentation'] = resized_rle
+
+        resized_annotations.append(resized_annotation)
+
+    mask = np.array(masks[0])
+    for i in range(1, len(masks)):
+        mask = mask | np.array(masks[i])
+
+    # Convert masks to numpy array
+    # masks_array = np.array(masks)
+    mask = np.array(mask)
+
+    return mask, file_name
+
+
+def preprocess_catpp():
     """Preprocess cat pictures into parquet files for speed and space efficiency"""
 
     # Checks
@@ -38,8 +113,10 @@ def preprocess_catpp(root, target_size):
                 f"Folder {folders[i]} at {p} does not exist.")
 
     # Preprocess images and segmentations
+    num_data = [0, 0, 0]
+
     transform = transforms.Compose([
-        transforms.Resize((224, 224),
+        transforms.Resize(target_size,
                           interpolation=transforms.InterpolationMode.BILINEAR),
         # transforms.ToTensor(),
         # transforms.Normalize(
@@ -60,6 +137,8 @@ def preprocess_catpp(root, target_size):
         all_images_path = list(p.glob("*.jpg"))
         all_json_path = list(p.glob("*.json"))
 
+        rows = []
+        rows_pickle = []
         errors = []
 
         print("### Number of files ###", "\nImages:", len(all_images_path),
@@ -70,79 +149,47 @@ def preprocess_catpp(root, target_size):
         out_dir = root_path / f"{folders[i]}_mask"
         out_dir.mkdir(parents=True, exist_ok=True)
 
-        # load images
-        for j in range(min_len):
+        # load data
+        for j in tqdm(range(min_len)):
             img_path = all_images_path[j]
             json_path = all_json_path[j]
 
+            # images
             img = Image.open(img_path)
-            with open(json_path, 'r') as file:
-                data = json.load(file)
 
-            img = transform(img)
+            img = np.array(transform(img))
 
-            annotations = data.get('annotations', [])
-            masks = []
-            resized_annotations = []
+            # json
+            mask, file_name = process_segmentation(json_path)
 
-            image_info = data.get('image', {})
-            image_id = image_info.get('image_id')
-            file_name = image_info.get('file_name')
-            original_height = image_info.get('height')
-            original_width = image_info.get('width')
+            if out_dir and len(mask) != 0:
+                mask_path = os.path.join(out_dir, file_name)
+                mask_image = Image.fromarray((mask * 255).astype(np.uint8))
+                mask_image.save(mask_path)
 
-            resize_factor_x = target_size[0] / original_width
-            resize_factor_y = target_size[1] / original_height
-            
-            for annotation in annotations:
-                annotation_id = annotation.get('id')
-                
-                # Decode RLE segmentation
-                rle = annotation.get('segmentation')
-                binary_mask = mask_utils.decode(rle)
-                
-                # Convert to PIL Image and resize
-                mask_image = Image.fromarray(binary_mask.astype(np.uint8) * 255)
-                resized_mask = mask_image.resize(target_size, Image.NEAREST)
-                
-                # Convert back to numpy array and normalize
-                resized_mask_array = np.array(resized_mask) / 255
-                masks.append(resized_mask_array)
-                
-                # Resize bounding box
-                original_bbox = annotation.get('bbox')
-                resized_bbox = [
-                    original_bbox[0] * resize_factor_x,
-                    original_bbox[1] * resize_factor_y,
-                    original_bbox[2] * resize_factor_x,
-                    original_bbox[3] * resize_factor_y
-                ]
-                
-                # Update annotation with resized values
-                resized_annotation = annotation.copy()
-                resized_annotation['bbox'] = resized_bbox
-                resized_annotation['area'] = annotation.get('area') * resize_factor_x * resize_factor_y
-                
-                # Create RLE for the resized mask
-                resized_mask_binary = resized_mask_array.astype(np.uint8)
-                resized_rle = mask_utils.encode(np.asfortranarray(resized_mask_binary))
-                resized_rle['counts'] = resized_rle['counts'].decode('utf-8')
-                resized_annotation['segmentation'] = resized_rle
-                
-                resized_annotations.append(resized_annotation)
-                
-                # Save individual mask if output directory is specified
-                
-                if out_dir:
-                    mask_path = os.path.join(out_dir, f"mask_{annotation_id}.png")
-                    mask_image = Image.fromarray((resized_mask_array * 255).astype(np.uint8))
-                    mask_image.save(mask_path)
-            
-            # Convert masks to numpy array
-            masks_array = np.array(masks)
+                num_data[i] += 1
+
+                rows.append([*img.flatten(), *mask.flatten()])
+                rows_pickle.append([pickle.dumps(img), pickle.dumps(mask)])
 
         if len(errors) == 0:
-            print(f"### No errors for {folders[i]} ###\n")
+            if rows:
+                rows = np.array(rows)
+
+                pl.DataFrame(rows).write_parquet(
+                    f"{root_path}/{folders[i]}_dataset.parquet")
+
+                pl.DataFrame(rows).write_csv(
+                    f"{root_path}/{folders[i]}_dataset.csv",
+                    include_header=False,
+                    include_bom=False)
+
+                with open(f"{root_path}/{folders[i]}_dataset.pkl",
+                          "wb") as pkl_file:
+                    pickle.dump(rows_pickle, pkl_file)
+
+            print(
+                f"### No errors for {folders[i]} - {num_data[i]} masks ###\n")
         else:
             print(f"### Error found in {folders[i]} ###")
             print(errors, '\n')
@@ -151,4 +198,4 @@ def preprocess_catpp(root, target_size):
 if __name__ == '__main__':
     root = r"D:\storage\feral-cat-segmentation.v1i.sam2"
     target_size = [224, 224]
-    preprocess_catpp(root, target_size)
+    preprocess_catpp()
